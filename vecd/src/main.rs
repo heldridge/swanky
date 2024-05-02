4d131ae83ec128f66191f8be5b46d077df3ab8c4
Harry <hme@cs.jh.edu>
2024-05-02 14:39:27 -0400

    Load circuit from argument

--- vecd/src/main.rs
+++ vecd/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::{fs::File, io::BufReader};
+
+use fancy_garbling::circuit::BinaryCircuit as Circuit;
+
+fn circuit(fname: &str) -> Circuit {
+    println!("* Circuit {}", fname);
+    Circuit::parse(BufReader::new(File::open(fname).unwrap())).unwrap()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Start");
+
+    let args: Vec<String> = env::args().collect();
+    let circuit_file_name = &args[1];
+
+    let mut _circ = circuit(circuit_file_name);
 }
